864f24988991a2385ec9e941623ab3c55055c46b
Jeremy Morales <jeremy.morales@gmail.com>
2020-06-27 12:10:09 -0700

    store added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function reducer() {
+    return {
+        dimensions: {
+            cols: 50,
+            rows: 25,
+        },
+    };
+}
+
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        {' '}
+        <App />
+    </Provider>,
+
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
