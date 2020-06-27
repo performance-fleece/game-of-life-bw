a09266830f81b9c44e36c98d29ff3c9d877fce20
Jeremy Morales <jeremy.morales@gmail.com>
2020-06-27 10:54:41 -0700

    basic map and general layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Cell from './components/Cell';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            cols: 50,
+            rows: 25,
+        };
+    }
+
+    renderMap() {
+        let newMap = [];
+        let cellRow = [];
+
+        for (let i = 0; i < this.state.cols; i++) {
+            for (let j = 0; j < this.state.rows; j++) {
+                cellRow.push(<Cell key={(i, j)} />);
+            }
+            newMap.push(
+                <div className="row" key={i}>
+                    {cellRow}
+                </div>
+            );
+            cellRow = [];
+        }
+        return newMap;
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <div className="upper-wrapper">
+                    <div className="game-box">{this.renderMap()}</div>
+                    <div className="pre-mades">pre-mades here</div>
+                </div>
+                <div className="lower-wrapper">
+                    <div className="game-description"></div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Cell.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Cell() {
+    return <div className="cellBox"></div>;
+}
+
+export default Cell;
