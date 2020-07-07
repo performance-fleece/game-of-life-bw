f0e34a25aa6f9259fac1099c4c862cbd7c2b6a3a
Jeremy Morales <jeremy.morales@gmail.com>
2020-07-07 16:01:50 -0700

    remove extra redux file

--- src/components/Helpers.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export const useInput = (initialValue) => {
-    const [value, setValue] = useState(initialValue);
-    const handleChanges = (updatedValue) => {
-        setValue(updatedValue);
-    };
-    return [value, setValue, handleChanges];
-};
