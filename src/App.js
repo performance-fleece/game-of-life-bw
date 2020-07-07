27568b5c327583fb10811e9ec0c94ec7df3ceb7d
Jeremy Morales <jeremy.morales@gmail.com>
2020-07-07 16:02:11 -0700

    Initial App layout with placeholder game component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,109 @@
 import React, { Component } from "react";
 import "./App.css";
-import Cell from "./components/Cell";
-import CustomForm from "./components/Form";
 
 export default class App extends Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 		this.state = {
-			cols: 50,
-			rows: 25,
+			size: [50, 25],
 		};
 	}
 
-	renderMap() {
-		let newMap = [];
+	renderGame() {
+		let newBoard = [];
 		let cellRow = [];
 
-		for (let i = 0; i < this.state.cols; i++) {
-			for (let j = 0; j < this.state.rows; j++) {
-				cellRow.push(<Cell key={(i, j)} />);
+		for (let i = 0; i < this.state.size[0]; i++) {
+			for (let j = 0; j < this.state[1]; j++) {
+				if (this.state.game.isCellAlive(i + " , " + j)) {
+					cellRow.push(
+						<Cell
+							key={(i, j)}
+							position={{ x: i, y: j }}
+							live={true}
+							updateCell={this.updateCell.bind(this)}
+						/>
+					);
+				} else {
+					cellRow.push(
+						<Cell
+							key={(i, j)}
+							position={{ x: i, y: j }}
+							live={false}
+							updateCell={this.updateCell.bind(this)}
+						/>
+					);
+				}
 			}
-			newMap.push(
+
+			newBoard.push(
 				<div className="row" key={i}>
 					{cellRow}
 				</div>
 			);
 			cellRow = [];
 		}
-		return newMap;
+		return newBoard;
 	}
+
+	handleRowChange() {}
+
+	handleColumnChange() {}
+
+	startGame() {}
+	stopGame() {}
+
+	getGeneration() {}
 
 	render() {
 		return (
 			<div className="main">
-				<div className="upper-wrapper">
-					<div className="game-box">{this.renderMap()}</div>
-					<div className="pre-mades">pre-mades here</div>
+				<div className="headerWrapper">
+					<div className="headerInner">
+						<label className="label">
+							Rows:
+							<input
+								className="input"
+								type="text"
+								value={this.state.size[1]}
+								onChange={this.handleRowChange}
+							/>
+						</label>
+						<label className="label">
+							Rows:
+							<input
+								className="input"
+								type="text"
+								value={this.state.size[0]}
+								onChange={this.handleColumnChange}
+							/>
+						</label>
+					</div>
+					<div className="headerButtons">
+						<button className="submit" onClick={this.startGame}>
+							Start
+						</button>
+						<button className="submit" onClick={this.stopGame}>
+							Stop
+						</button>
+					</div>
+					Generation: {this.state.game.getGeneration()}
 				</div>
-				<div className="lower-wrapper">
-					<CustomForm />
-					<div className="game-description"></div>
-				</div>
+				<div className="boardContainer">{this.renderGame()}</div>
 			</div>
+		);
+	}
+}
+
+class Cell extends Component {
+	render() {
+		return (
+			<div
+				onClick={() => this.props.storeCell(this.props.position)}
+				className={
+					this.props.live ? "cellContainerLive" : "cellContainerDead"
+				}
+			></div>
 		);
 	}
 }
