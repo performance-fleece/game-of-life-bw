083c47d568fc683af7b67ce89194b2265baf979e
Jeremy Morales <jeremy.morales@gmail.com>
2020-07-07 15:40:23 -0700

    more basic cleanup

--- src/App.js
+++ src/App.js
@@ -1,50 +1,47 @@
-import React, { useState } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import Cell from './components/Cell';
-import CustomForm from './components/Form';
+import React, { Component } from "react";
+import "./App.css";
+import Cell from "./components/Cell";
+import CustomForm from "./components/Form";
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            cols: 50,
-            rows: 25,
-        };
-    }
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			cols: 50,
+			rows: 25,
+		};
+	}
 
-    renderMap() {
-        let newMap = [];
-        let cellRow = [];
+	renderMap() {
+		let newMap = [];
+		let cellRow = [];
 
-        for (let i = 0; i < this.state.cols; i++) {
-            for (let j = 0; j < this.state.rows; j++) {
-                cellRow.push(<Cell key={(i, j)} />);
-            }
-            newMap.push(
-                <div className="row" key={i}>
-                    {cellRow}
-                </div>
-            );
-            cellRow = [];
-        }
-        return newMap;
-    }
+		for (let i = 0; i < this.state.cols; i++) {
+			for (let j = 0; j < this.state.rows; j++) {
+				cellRow.push(<Cell key={(i, j)} />);
+			}
+			newMap.push(
+				<div className="row" key={i}>
+					{cellRow}
+				</div>
+			);
+			cellRow = [];
+		}
+		return newMap;
+	}
 
-    render() {
-        return (
-            <div className="main">
-                <div className="upper-wrapper">
-                    <div className="game-box">{this.renderMap()}</div>
-                    <div className="pre-mades">pre-mades here</div>
-                </div>
-                <div className="lower-wrapper">
-                    <CustomForm />
-                    <div className="game-description"></div>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="main">
+				<div className="upper-wrapper">
+					<div className="game-box">{this.renderMap()}</div>
+					<div className="pre-mades">pre-mades here</div>
+				</div>
+				<div className="lower-wrapper">
+					<CustomForm />
+					<div className="game-description"></div>
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
